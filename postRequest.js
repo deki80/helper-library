60fed3305426fe0fbadcd4a8a70fa1d19287dc63
Dejan <dejan.tomic@yahoo.com>
2019-06-23 16:20:35 +0200

    helper function for post requests

---
+++ postRequest.js
@@ -0,0 +1,15 @@
+export default function postData (url, data, contentType = 'json', auth = false) {
+    contentType = contentType === 'form' ? 'application/x-www-form-urlencoded' : 'application/json'
+    // data = contentType === 'json' ? JSON.stringify(data) : data
+    data = JSON.stringify(data)
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': `${contentType}`,
+        'auth': `${auth}`
+      },
+      body: data
+    })
+      .then(response => response.json())
+  }
+  
